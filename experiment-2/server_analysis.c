8828a7614384969ffe0e767ef9ed5c006d3f51cd
rlad78 <goobysc@gmail.com>
2018-05-02 00:59:38 -0400

    visual version

--- experiment-2/server_analysis.c
+++ experiment-2/server_analysis.c
@@ -33,6 +33,84 @@ struct DataPacket
     int status;
 };
 
+const unsigned int SLEEP_TIME = 50;
+
+/* returns a string of n length with n-1 spaces and end-terminated
+ * with a NULL char
+ */
+char * blankStr(unsigned int n){
+    int i;
+    char *string;
+    string = (char*) malloc(sizeof(char)*n);
+    for (i = 0; i < n-1; i++)
+    {
+        string[i] = ' ';
+    }
+    string[n-1] = '\0';
+    return string;
+}
+
+/* prints to stdout a graphical representation of car positions
+ */
+void print_vehicle(int lane_print[4], int pos[4]){
+    int i, startpoint = 0, endpoint = 75;
+    char *lane1;
+    char *lane2;
+    lane1 = blankStr(81);
+    lane2 = blankStr(81);
+
+    system("clear");
+
+    for (i = 0; i < 4; i++)
+    {
+        if (pos[i] > endpoint)
+        {
+            endpoint = pos[i] + 5;
+            startpoint = endpoint - 75;
+        }
+
+        switch(lane_print[i]){
+            case 1 : 
+                lane1[pos[i]] = i + '0';
+                lane1[pos[i]-1] = '[';
+                lane1[pos[i]+1] = ']';
+                break;
+            case 2 :
+                lane2[pos[i]] = i + '0';
+                lane2[pos[i]-1] = '[';
+                lane2[pos[i]+1] = ']';
+                break;
+        }
+
+    }
+
+    for (i = 0; i < 16; i++)
+    {
+        puts("|----");
+    }
+    putc('\n');
+    puts(lane1);
+    putc('\n');
+    for (i = 0; i < 16; i++)
+    {
+        puts("-----");
+    }
+    putc('\n');
+    puts(lane2);
+    for (i = 0; i < 16; i++)
+    {
+        puts("|----");
+    }
+    putc('\n');
+    while(startpoint < endpoint){
+        printf("%2d   ", startpoint);
+        startpoint += 5;
+    }
+
+    free(lane1);
+    free(lane2);
+}
+
 int main()
 {
     struct DataPacket read_packet;
@@ -47,7 +125,8 @@ int main()
     vehicles[3] = fopen("vehicle-4.v2v-exp2.ch-geni-net.instageni.stanford.edu", "rb");
 
     int vehicle_position[4][100];
-    int vehicle_lane [4];
+    int vehicle_lane[4][100];
+    int lane_print[4];
 
     for (i = 0; i < 4; i++)
     {
@@ -63,7 +142,7 @@ int main()
         {
             fread(&read_packet, sizeof(read_packet), 1, vehicles[i]);
             vehicle_position[i][read_packet.time] = read_packet.distance;
-            vehicle_lane[i] = read_packet.lane;
+            vehicle_lane[i][read_packet.time] = read_packet.lane;
         }
     }
 
@@ -71,17 +150,22 @@ int main()
 
     for (i = 0; i < 100; i++)
     {
+        lane_print = {0,0,0,0};
         for (j = 0; j < 4; j++)
         {
+            //store the vehicle number in lane print
+            lane_print[j] = vehicle_lane[j][i];
             for (k = j+1; k < 4; k++)
             {
-                if ((vehicle_position[j][i] == vehicle_position[k][i]) && (vehicle_position[j][i] != 0 && vehicle_position[k][i] != 0) && (vehicle_lane[j] == vehicle_lane[k]))
+                if ((vehicle_position[j][i] == vehicle_position[k][i]) && (vehicle_position[j][i] != 0 && vehicle_position[k][i] != 0) && (vehicle_lane[j][i] == vehicle_lane[k][i]))
                 {
                     printf("Vehicle %i collided with vehicle %i at %i seconds", j, k, i);
                     exit(1);
                 }
             }
         }
+        print_vehicle(lane_print,{vehicle_position[0][i],vehicle_position[1][i],vehicle_position[2][i],vehicle_position[3][i]});
+        usleep(SLEEP_TIME);
     }
 
 return 0;
